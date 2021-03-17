d88e1b6ad03b45913b0cb705b8e48257d08527b2
Kryspin Trawnik <ktrawnik@gmail.com>
2021-03-17 21:33:37 +0000

    first commit

---
+++ RestaurantMVC.Web/Models/Deal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantMVC.Web.Models
+{
+    public class Deal
+    {
+        int Id { get; set; }
+
+        string Name { get; set; }
+
+        double Price { get; set; }
+
+        List<Product> Products { get; set; }
+    }
+}
---
+++ RestaurantMVC.Web/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantMVC.Web.Models
+{
+    public class Product
+    {
+        int Id { get; set; }
+
+        string Name { get; set; }
+
+        double Price { get; set; }
+
+        string Description { get; set; }
+
+        int Type { get; set; }
+
+
+    }
+}
